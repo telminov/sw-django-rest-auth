5f2ba265e43112b064542d15a3ca8405389e0166
sergey@telminov.ru <sergey@telminov.ru>
2016-02-22 14:53:31 +0300

    check perm view tests

--- sw_rest_auth/tests/test_views.py
+++ sw_rest_auth/tests/test_views.py
@@ -22,3 +22,21 @@ class CheckToken(AuthTestCaseMixin, test.APITestCase):
     def test_invalid_key(self):
         response = self.client.post(self.url, {'token': 'not_existing_token_key'})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class CheckPerm(AuthTestCaseMixin, test.APITestCase):
+    url = reverse(views.check_perm)
+
+    def setUp(self):
+        super(CheckPerm, self).setUp()
+        self.permission = models.Permission.objects.create(code='code', name='name', description='description')
+
+    def test_have_perm(self):
+        models.UserPermission.objects.create(permission=self.permission, user=self.get_user())
+        response = self.client.get(self.url, {'user': self.get_user().username, 'perm': self.permission.code})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_have_no_perm(self):
+        models.UserPermission.objects.filter(permission=self.permission, user=self.get_user()).delete()
+        response = self.client.get(self.url, {'user': self.get_user().username, 'perm': self.permission.code})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
