757b43cb171b6e00cdde8c4514adb741cd3eee9e
sergey@telminov.ru <sergey@telminov.ru>
2016-05-06 12:28:51 +0300

    add possibility to check permission via static method

--- sw_rest_auth/permissions.py
+++ sw_rest_auth/permissions.py
@@ -14,12 +14,19 @@ class CodePermission(permissions.BasePermission):
         if not (request.user and request.user.username):
             return False
 
+        return self.has_permission_by_params(
+            username=request.user.username,
+            permission_code=self.permission_code
+        )
+
+    @staticmethod
+    def has_permission_by_params(username, permission_code, raise_exception=True):
         url = settings.AUTH_SERVICE_CHECK_PERM_URL
         auth_token = settings.AUTH_TOKEN
         auth_verified_ssl_crt = getattr(settings, 'AUTH_VERIFIED_SSL_CRT_PATH', None)
 
         headers = {'Authorization': 'Token %s' % auth_token}
-        params = {'user': request.user.username, 'perm': self.permission_code}
+        params = {'user': username, 'perm': permission_code}
         try:
             kwargs = {
                 'headers': headers,
@@ -28,12 +35,19 @@ class CodePermission(permissions.BasePermission):
             if auth_verified_ssl_crt:
                 kwargs['verify'] = auth_verified_ssl_crt
             r = requests.get(url, **kwargs)
+
         except requests.ConnectionError:
-            raise PermissionDenied(detail='Can not connect to authorization service')
+            if raise_exception:
+                raise PermissionDenied(detail='Can not connect to authorization service')
+            else:
+                return False
 
         if r.status_code != 200:
-            detail = '; '.join(['%s: %s' % (k, ', '.join(v)) for k, v in r.json().items()])
-            raise PermissionDenied(detail=detail)
+            if raise_exception:
+                detail = '; '.join(['%s: %s' % (k, ', '.join(v)) for k, v in r.json().items()])
+                raise PermissionDenied(detail=detail)
+            else:
+                return False
 
         return True
 
